8538c68352e7ceadf30eb05f389ad39bb083abda
tdeakin <amles.gamer@gmail.com>
2016-09-01 20:48:16 +1200

    Added home-page template

---
+++ page-home.php
@@ -0,0 +1,7 @@
+<?php theme_include('header'); ?>
+    <div class="row">
+        <div class="col-sm-8">
+            <?php echo page_content(); ?>
+        </div>
+    </div>
+<?php theme_include('footer'); ?>
