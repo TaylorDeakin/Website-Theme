cc66ae4c76d65026c0ace1abe41d74ce9f038952
tdeakin <amles.gamer@gmail.com>
2016-09-11 18:28:26 +1200

    fixing og_url should probably test this more before pushing next time

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 
     <meta property="og:title" content="<?php echo og_title(); ?>">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="<?php echo e(current_url()); ?>">
+    <meta property="og:url" content="<?php echo "http://taylordeakinme/" . e(current_url()); ?>">
     <meta property="og:site_name" content="<?php echo site_name(); ?>">
     <meta property="og:description" content="<?php echo og_description(); ?>">
     <meta property="og:image" content="<?php echo og_image();?>">
